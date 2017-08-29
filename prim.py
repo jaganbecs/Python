2dbe195ccf4f253d9885131f3588612642d70572
Jaganathan M <31428641+jaganbecs@users.noreply.github.com>
2017-08-29 15:19:47 +0530

    Create prim.py

---
+++ prim.py
@@ -0,0 +1,13 @@
+l=50
+h=100
+flag=0
+print("prime nos. between",low,"and",up,"are")
+for num in range(l,h+1):
+if num>1
+for i in range(2,num):
+if(num%i)==0:
+break
+else:
+print(num)
+flag=flag+1
+print(flag)
